4c14c06ca8bf79aba653465595468ea0fb4a63ba
Nicolas Eeckeloo <neeckeloo@gmail.com>
2017-09-18 13:43:16 +0200

    Fix service name used to retrieve EventManager

--- src/Container/IdempotentRequestListenerFactory.php
+++ src/Container/IdempotentRequestListenerFactory.php
@@ -6,13 +6,12 @@ namespace Riskio\IdempotencyModule\Container;
 use Interop\Container\ContainerInterface;
 use Riskio\IdempotencyModule\IdempotentRequestListener;
 use Riskio\IdempotencyModule\IdempotencyService;
-use Zend\EventManager\EventManager;
 
 final class IdempotentRequestListenerFactory
 {
     public function __invoke(ContainerInterface $container) : IdempotentRequestListener
     {
-        $eventManager = $container->get(EventManager::class);
+        $eventManager = $container->get('EventManager');
         $idempotencyService = $container->get(IdempotencyService::class);
 
         return new IdempotentRequestListener($eventManager, $idempotencyService);
